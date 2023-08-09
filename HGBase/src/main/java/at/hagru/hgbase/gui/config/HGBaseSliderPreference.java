1c11b993bfb1229bf33b6a26af96137d49897867
josef <josef.jordan@gmx.net>
2023-08-09 17:14:18 +0200

    Reformatted code.

--- HGBase/src/main/java/at/hagru/hgbase/gui/config/HGBaseSliderPreference.java
+++ HGBase/src/main/java/at/hagru/hgbase/gui/config/HGBaseSliderPreference.java
@@ -14,116 +14,116 @@ import at.hagru.hgbase.lib.HGBaseText;
 
 /**
  * A number slider preference that uses the android seek bar.
- * 
+ *
  * @author hagru
  */
 public class HGBaseSliderPreference extends DialogPreference implements SeekBar.OnSeekBarChangeListener {
-	
-	private Activity activity;
-	private SeekBar seekBar;
-	private TextView txtValue;
-	private int currentValue;
-	private int minValue;
-	private int maxValue;
-	private String unitText;
 
-	public HGBaseSliderPreference(Activity activity, int minValue, int maxValue, String unitText) {
-		super(activity, null);
-		this.activity = activity;
-		this.minValue = minValue;
-		this.maxValue = maxValue;
-		this.unitText = (unitText == null) ? "" : unitText;
-		setDefaultValue(Integer.valueOf(minValue));
-	}
+    private Activity activity;
+    private SeekBar seekBar;
+    private TextView txtValue;
+    private int currentValue;
+    private int minValue;
+    private int maxValue;
+    private String unitText;
 
-	@Override
-	protected View onCreateDialogView() {
-		currentValue = getValue();
-		LinearLayout mainPanel = HGBaseGuiTools.createLinearLayout(activity, false);
-		if (HGBaseText.existsText(getKey())) {
-			TextView txtTitel  = HGBaseGuiTools.createViewForMessage(activity, HGBaseText.getText(getKey()));
-			mainPanel.addView(txtTitel, HGBaseGuiTools.createLinearLayoutParams(true, false));
-		}
-		txtValue = HGBaseGuiTools.createViewForMessage(activity, "");
-		mainPanel.addView(txtValue, HGBaseGuiTools.createLinearLayoutParams(true, false));
-		txtValue.setGravity(Gravity.CENTER);
-		actualizeValue();
-		seekBar = new SeekBar(activity);
-		seekBar.setMax(maxValue - minValue);
-		seekBar.setProgress(currentValue - minValue);
-		seekBar.setOnSeekBarChangeListener(this);
-		mainPanel.addView(seekBar, HGBaseGuiTools.createLinearLayoutParams(true, false));
-		return mainPanel;
-	}
-	
-	@Override
-	public void setDefaultValue(Object defaultValue) {
-		super.setDefaultValue(((Integer) defaultValue).intValue() - minValue);
-	}
-	
-	/**
-	 * Actualizes the value, including the text of the unit
-	 */
-	protected void actualizeValue() {
-		if (txtValue != null) {
-			txtValue.setText(String.valueOf(currentValue) + " " + unitText);
-		}
-	}
-	
-	@Override
-	public void onProgressChanged(SeekBar seek, int value, boolean fromTouch) {
-		currentValue = value + minValue;
-		actualizeValue();
-	}
-	
-	@Override
-	protected void onDialogClosed(boolean positiveResult) {
-		super.onDialogClosed(positiveResult);
-		if (positiveResult) {
-			setValue(currentValue);
-		}
-	}
+    public HGBaseSliderPreference(Activity activity, int minValue, int maxValue, String unitText) {
+        super(activity, null);
+        this.activity = activity;
+        this.minValue = minValue;
+        this.maxValue = maxValue;
+        this.unitText = (unitText == null) ? "" : unitText;
+        setDefaultValue(Integer.valueOf(minValue));
+    }
 
-	@Override
-	public void onStartTrackingTouch(SeekBar seek) {
-		//NOCHECK: nothing to do by default
-	}
+    @Override
+    protected View onCreateDialogView() {
+        currentValue = getValue();
+        LinearLayout mainPanel = HGBaseGuiTools.createLinearLayout(activity, false);
+        if (HGBaseText.existsText(getKey())) {
+            TextView txtTitel = HGBaseGuiTools.createViewForMessage(activity, HGBaseText.getText(getKey()));
+            mainPanel.addView(txtTitel, HGBaseGuiTools.createLinearLayoutParams(true, false));
+        }
+        txtValue = HGBaseGuiTools.createViewForMessage(activity, "");
+        mainPanel.addView(txtValue, HGBaseGuiTools.createLinearLayoutParams(true, false));
+        txtValue.setGravity(Gravity.CENTER);
+        actualizeValue();
+        seekBar = new SeekBar(activity);
+        seekBar.setMax(maxValue - minValue);
+        seekBar.setProgress(currentValue - minValue);
+        seekBar.setOnSeekBarChangeListener(this);
+        mainPanel.addView(seekBar, HGBaseGuiTools.createLinearLayoutParams(true, false));
+        return mainPanel;
+    }
 
-	@Override
-	public void onStopTrackingTouch(SeekBar seek) {
-		//NOCHECK: nothing to do by default
-	}
-	
-	/**
-	 * @return the current value
-	 */
-	public int getValue() {
-		return (HGBaseConfig.existsKey(getKey())) ? HGBaseConfig.getInt(getKey()) : minValue;
-	}
+    @Override
+    public void setDefaultValue(Object defaultValue) {
+        super.setDefaultValue(((Integer) defaultValue).intValue() - minValue);
+    }
 
-	/**
-	 * @param value the new value to set
-	 */
-	public void setValue(int value) {
-		currentValue = value;
-		HGBaseConfig.set(getKey(), value);
-		if (seekBar != null) {
-			seekBar.setProgress(value - minValue);
-		}
-		actualizeValue();
-	}
+    /**
+     * Actualizes the value, including the text of the unit
+     */
+    protected void actualizeValue() {
+        if (txtValue != null) {
+            txtValue.setText(String.valueOf(currentValue) + " " + unitText);
+        }
+    }
 
-	/**
-	 * @return the minimum value
-	 */
-	public int getMinValue() {
-		return minValue;
-	}
+    @Override
+    public void onProgressChanged(SeekBar seek, int value, boolean fromTouch) {
+        currentValue = value + minValue;
+        actualizeValue();
+    }
 
-	/**
-	 * @return the maximum value
-	 */
-	public int getMaxValue() {
-		return maxValue;
-	}
+    @Override
+    protected void onDialogClosed(boolean positiveResult) {
+        super.onDialogClosed(positiveResult);
+        if (positiveResult) {
+            setValue(currentValue);
+        }
+    }
+
+    @Override
+    public void onStartTrackingTouch(SeekBar seek) {
+        //NOCHECK: nothing to do by default
+    }
+
+    @Override
+    public void onStopTrackingTouch(SeekBar seek) {
+        //NOCHECK: nothing to do by default
+    }
+
+    /**
+     * @return the current value
+     */
+    public int getValue() {
+        return (HGBaseConfig.existsKey(getKey())) ? HGBaseConfig.getInt(getKey()) : minValue;
+    }
+
+    /**
+     * @param value the new value to set
+     */
+    public void setValue(int value) {
+        currentValue = value;
+        HGBaseConfig.set(getKey(), value);
+        if (seekBar != null) {
+            seekBar.setProgress(value - minValue);
+        }
+        actualizeValue();
+    }
+
+    /**
+     * @return the minimum value
+     */
+    public int getMinValue() {
+        return minValue;
+    }
+
+    /**
+     * @return the maximum value
+     */
+    public int getMaxValue() {
+        return maxValue;
+    }
 }

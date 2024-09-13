9224b714ffdb5b74d213b54d020c53f14eb8ac00
Josef Jordan <josef.jordan@gmx.net>
2024-09-13 17:03:06 +0200

    Documentation and formatting.

--- HGBase/src/main/java/at/hagru/hgbase/lib/Pair.java
+++ HGBase/src/main/java/at/hagru/hgbase/lib/Pair.java
@@ -2,7 +2,7 @@ package at.hagru.hgbase.lib;
 
 /**
  * A pair with two objects. Objects may be null.
- * 
+ *
  * @param <F> type of the first object
  * @param <S> type of the second object
  * @author hagru
@@ -10,15 +10,20 @@ package at.hagru.hgbase.lib;
  */
 @Deprecated
 public class Pair<F, S> {
-    
+    /**
+     * The first object.
+     */
     private F first;
+    /**
+     * The second object.
+     */
     private S second;
 
     /**
      * Create a new pair of objects.
-     * 
-     * @param first the first object, may be null
-     * @param second the second object, may be null
+     *
+     * @param first  the first object, may be {@code null}
+     * @param second the second object, may be {@code null}
      */
     public Pair(F first, S second) {
         this.first = first;
@@ -27,8 +32,8 @@ public class Pair<F, S> {
 
     /**
      * Returns the first object.
-     * 
-     * @return the first object, may be null
+     *
+     * @return the first object, may be {@code null}
      */
     public F getFirst() {
         return first;
@@ -36,8 +41,8 @@ public class Pair<F, S> {
 
     /**
      * Returns the second object.
-     * 
-     * @return the second object, may be null
+     *
+     * @return the second object, may be {@code null}
      */
     public S getSecond() {
         return second;
@@ -48,15 +53,15 @@ public class Pair<F, S> {
      */
     @Override
     public boolean equals(Object o2) {
-        if (o2 instanceof Pair<?,?>) {
-            Pair<?,?> p2 = (Pair<?,?>) o2;
+        if (o2 instanceof Pair<?, ?>) {
+            Pair<?, ?> p2 = (Pair<?, ?>) o2;
             return HGBaseTools.equalObject(getFirst(), p2.getFirst())
                     && HGBaseTools.equalObject(getSecond(), p2.getSecond());
         } else {
             return false;
         }
     }
-    
+
     /* (non-Javadoc)
      * @see java.lang.Object#hashCode()
      */
@@ -64,5 +69,4 @@ public class Pair<F, S> {
     public int hashCode() {
         return HGBaseTools.hashCode(getFirst()) * 31 + HGBaseTools.hashCode(getSecond());
     }
-    
 }
